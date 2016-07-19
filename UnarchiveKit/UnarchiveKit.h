3358f70e8073b2c5f39b611d228282f739b109a5
James Lawton <james.lawton@gmail.com>
2016-07-19 11:08:06 -0700

    Initial Commit

---
+++ UnarchiveKit/UnarchiveKit.h
@@ -0,0 +1,19 @@
+//
+//  UnarchiveKit.h
+//  UnarchiveKit
+//
+//  Created by James Lawton on 7/19/16.
+//  Copyright © 2016 James Lawton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UnarchiveKit.
+FOUNDATION_EXPORT double UnarchiveKitVersionNumber;
+
+//! Project version string for UnarchiveKit.
+FOUNDATION_EXPORT const unsigned char UnarchiveKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnarchiveKit/PublicHeader.h>
+
+

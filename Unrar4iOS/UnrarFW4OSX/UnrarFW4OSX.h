9aaaa0460a06da76e33b79ab29895953f4779793
James Lawton <james.lawton@gmail.com>
2016-07-20 13:43:36 -0700

    Squashed 'UnarchiveKit/Unrar4iOS/' content from commit 3959e9e
    git-subtree-dir: UnarchiveKit/Unrar4iOS
    git-subtree-split: 3959e9e2a5834cb019f4327405fd00dbdeb55520

---
+++ Unrar4iOS/UnrarFW4OSX/UnrarFW4OSX.h
@@ -0,0 +1,19 @@
+//
+//  UnrarFW4OSX.h
+//  UnrarFW4OSX
+//
+//  Created by Lukáš Foldýna on 23/11/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for UnrarFW4OSX.
+FOUNDATION_EXPORT double UnrarFW4OSXVersionNumber;
+
+//! Project version string for UnrarFW4OSX.
+FOUNDATION_EXPORT const unsigned char UnrarFW4OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnrarFW4OSX/PublicHeader.h>
+
+
---
+++ Unrar4iOS/UnrarFW4iOS/UnrarFW4iOS.h
@@ -0,0 +1,19 @@
+//
+//  UnrarFW4iOS.h
+//  UnrarFW4iOS
+//
+//  Created by Lukáš Foldýna on 23/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UnrarFW4iOS.
+FOUNDATION_EXPORT double UnrarFW4iOSVersionNumber;
+
+//! Project version string for UnrarFW4iOS.
+FOUNDATION_EXPORT const unsigned char UnrarFW4iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnrarFW4iOS/PublicHeader.h>
+
+

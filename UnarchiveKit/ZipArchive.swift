1d856180d0e33abe87f74a9a68f9cd6243f2e343
James Lawton <james.lawton@gmail.com>
2016-07-19 23:57:42 -0700

    Import minizip into Swift

---
+++ UnarchiveKit/ZipArchive.swift
@@ -0,0 +1,14 @@
+//
+//  ZipArchive.swift
+//  UnarchiveKit
+//
+//  Created by James Lawton on 7/19/16.
+//  Copyright © 2016 James Lawton. All rights reserved.
+//
+
+import Foundation
+import minizip
+
+class ZipArchive {
+
+}

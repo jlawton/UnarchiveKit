16799b99e22cc0499b3f68705791a61867c3ff77
James Lawton <james.lawton@gmail.com>
2019-10-21 00:48:51 -0700

    Avoid deprecated override of withUnsafeBytes.

--- UnarchiveKit/TarBlockHeader.swift
+++ UnarchiveKit/TarBlockHeader.swift
@@ -22,16 +22,14 @@ struct TarBlockHeader {
         var blockType: BlockType = .Other
         var name: String = ""
         var size: Int = 0
-        data.withUnsafeBytes { (bytes: UnsafePointer<Int8>) in
+        data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) in
             // Check if this block is for a normal file or something else
             let type = bytes[156]
             if type == 0 {
-                for x in 0..<TarBlockSize {
-                    blockType = .Zero
-                    if bytes[x] != 0 {
-                        blockType = .Normal
-                        break
-                    }
+                blockType = .Zero
+                for x in bytes where x != 0 {
+                    blockType = .Normal
+                    break
                 }
             } else if type == 0x30 || type == 0x37 {
                 blockType = .Normal
@@ -39,19 +37,22 @@ struct TarBlockHeader {
                 blockType = .Other
             }
 
+            let bytesAsUInt8 = bytes.baseAddress!
+            let bytesAsInt8 = UnsafePointer<Int8>(OpaquePointer(bytesAsUInt8))
+
             // File size (octal)
             var sizeBytes = [Int8](repeating: 0, count: 13)
-            memcpy(&sizeBytes, bytes + 124, 12)
+            memcpy(&sizeBytes, bytesAsUInt8 + 124, 12)
             size = strtol(sizeBytes, nil, 8)
 
             // Name
             var nameBytes = [Int8](repeating: 0, count: 256)
-            strncpy(&nameBytes, bytes + 345, 155)
+            strncpy(&nameBytes, bytesAsInt8 + 345, 155)
             if nameBytes[0] != 0 {
                 // Add a slash after the name prefix
                 strncat(&nameBytes, [0x2F], 1)
             }
-            strncat(&nameBytes, bytes + 0, 100)
+            strncat(&nameBytes, bytesAsInt8 + 0, 100)
 
             if let nameString = String(validatingUTF8: nameBytes) {
                 name = nameString
